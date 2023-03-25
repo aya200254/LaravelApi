b93d662abdb1590c99dfb5b2f6fbdc6845b0182a
aya <ayqsherif@gmail.com>
2023-03-25 18:14:35 +0200

    updateing ProductController and user controller making routing

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -1,58 +1,53 @@
 <?php
 
 namespace App\Http\Controllers;
-use \App\Models\User;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\facades\Validator;
+namespace App\Http\Controllers;
 
-class userController extends Controller
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+class UserController extends Controller
 {
-    public function register(Request $request){
-
-        $validator=validator::make($request->all(),[
-            'email'=>'require | unique :users',
-            'password'=>'require',
-            'name'=>'require',
-            
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|unique:users',
+            'password' => 'required',
+            'name' => 'required',
         ]);
-        if($validator->fails()){
-    
-            return response()->json(['Error'=>$validator->errors()->all()],status:409);
-        }
-        $p=new User();
-        $p->name=$request->name;
-        $p->email=$request->email; 
-        $p->password=encrypt($request->password); 
-        $p->save();
-        return response()->json(['message'=>["Sucessfully Registered"]]);
 
-
-      
-    }
-   public function login(Request $request){
-
-    $validator=validator::make($request->all(),[
-        'email'=>'require ',
-         'password'=>'require', 
-
-    ]);
-    if($validator->fails()){
-
-        return response()->json(['Error'=>$validator->errors()->all()],status:409);
-    }
-   
-    $user=User::where('email',$request->email)->get()->first();
-    $password=bcrypt($user->password);
-    if($user && $password == $request->password ){
-        
-        return response()->json(['user'=>$user]);
-       
-    }
-        else{
-            return response()->json(['Error'=>["oops! Something Going Wrong"]],status:409);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->all()], 409);
         }
 
-   }
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
 
+        return response()->json(['message' => 'Successfully registered']);
+    }
 
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()->all()], 409);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && bcrypt($request->password) == $user->password) {
+            return response()->json(['user' => $user]);
+        } else {
+            return response()->json(['error' => 'Invalid credentials'], 409);
+        }
+    }
 }
--- app/Models/product.php
+++ app/Models/product.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class product extends Model
+class Product extends Model
 {
+    protected $table = 'products'; // replace with your actual table name
     use HasFactory;
 }
